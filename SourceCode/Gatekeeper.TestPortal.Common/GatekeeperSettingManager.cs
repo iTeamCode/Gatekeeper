63d4934b8214a7fe8bd38a21141b93c75fc3ff46
Alan luo <iteamalan@hotmail.com>
2015-11-03 18:07:58 +0800

    add common

---
+++ SourceCode/Gatekeeper.TestPortal.Common/GatekeeperSettingManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Gatekeeper.TestPortal.Common
+{
+    /// <summary>
+    /// Management
+    /// </summary>
+    public class GatekeeperSettingManager
+    {
+        public static string GetAppsetting(string settingName)
+        {
+            return ConfigurationManager.AppSettings[settingName];
+        }
+    }
+}
--- SourceCode/Gatekeeper.TestPortal.Dashboard/UnitTest1.cs
+++ SourceCode/Gatekeeper.TestPortal.Dashboard/UnitTest1.cs
@@ -6,6 +6,7 @@ namespace Gatekeeper.TestPortal.Dashboard
 {
     public class UnitTest1
     {
+        //https://dashboard.int2.fellowshipone.com/#/
         [Fact]
         public void TestMethod1()
         {
