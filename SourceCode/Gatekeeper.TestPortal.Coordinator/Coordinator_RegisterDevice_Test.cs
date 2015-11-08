b362030f6fe02a6f1292de826e545ae208128c59
Demi Zhang <demi.zhang@activenetwork.com>
2015-11-08 16:33:28 +0800

    Coordinator - Register Device

---
+++ SourceCode/Gatekeeper.TestPortal.Coordinator/Coordinator_RegisterDevice_Test.cs
@@ -0,0 +1,27 @@
+﻿using Gatekeeper.TestPortal.Common;
+using Gatekeeper.PageObject.Coordinator;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Gatekeeper.TestPortal.Coordinator
+{
+    public class Coordinator_RegisterDevice_Test
+    {
+        [Fact]
+        public void RegisterDevice_Success()
+        {
+            var manager = GatekeeperFactory.CreateDriverManager();
+            manager.NavigateTo(PageAlias.Coordinator_RegisterDevice);
+
+            var registerDevicePage = GatekeeperFactory.CreatePageManager<CoordinatorRegisterDevicePage>(manager.Driver);
+            registerDevicePage.AuthenticateChurch("ft.tester", "FT4life!", "dc");
+
+            WebElementKeeper.WaitingFor_UrlToBe(manager.Driver, PageAlias.Coordinator_ActivityCode);
+          
+        }
+
+        
+    }
+
+}
