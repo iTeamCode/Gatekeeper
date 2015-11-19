7a644b1fbda7d0b2d2c52a4a513253e52ac28f7c
Winnie Wang <winnie.wang@activenetwork.com>
2015-11-19 13:24:07 +0800

    Launchpad login error test

--- SourceCode/Gatekeeper.TestPortal.Launchpad/Launchpad_SignIn_Test.cs
+++ SourceCode/Gatekeeper.TestPortal.Launchpad/Launchpad_SignIn_Test.cs
@@ -24,4 +24,33 @@ namespace Gatekeeper.TestPortal.Launchpad
             manager.Driver.Close();
         }
     }
+
+    public class Launchpad_SignIn_Error_Test : IClassFixture<SingleBrowserFixture>
+    {
+        private IDriverManager _driverManager { get; set; }
+        private readonly ITestOutputHelper _output;
+        public Launchpad_SignIn_Error_Test(ITestOutputHelper output, SingleBrowserFixture fixture)
+        {
+            _driverManager = fixture.DriverManager;
+            _output = output;
+        }
+
+        [Theory]
+        [InlineData("", "", "Login attempt failed. Verify your information and try again.")]
+        //[InlineData("ft.tester", "", "", "Password is required.")]
+        //[InlineData("ft.tester", "FT4life!", "", "Church code is required.")]
+        public void SignIn_Error(string userName, string pwd, string errorMsg)
+        {
+            //Create manager & Navigate page to Login.
+            _driverManager.NavigateTo(PageAlias.Launchpad_SignIn);
+
+            var signInPage = GatekeeperFactory.CreatePageManager<LaunchpadSignInPage>(_driverManager.Driver);
+            signInPage.Action_SignIn(userName, pwd);
+
+            //Waiting & Check page.
+            var isPass = signInPage.Check_ErrorMessage(errorMsg);
+            _output.WriteLine("[Info]:Error message '{0}'", signInPage.ErrorMsg);
+            Assert.True(isPass, string.Format("error message is not '{0}'", errorMsg));
+        }
+    }
 }
