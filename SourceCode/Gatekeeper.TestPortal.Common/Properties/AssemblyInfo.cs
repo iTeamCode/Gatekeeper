35929a43e5913c3b94db2ff1e6b159331e2886bd
Alan luo <iteamalan@hotmail.com>
2015-12-02 17:09:42 +0800

    Update the Test Common

--- SourceCode/Gatekeeper.TestPortal.Common/Properties/AssemblyInfo.cs
+++ SourceCode/Gatekeeper.TestPortal.Common/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("52112700-55e5-48e4-9812-7b53d7d10779")]
+[assembly: Guid("9abbd6a9-5f95-442a-88db-3fc1ebf374a7")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ SourceCode/Gatekeeper.TestPortal.Common/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Gatekeeper.TestPortal.Common
+{
+    public class UnitTest1
+    {
+        //[Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
