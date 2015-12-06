ca4c4d1a06fc2b1ed3885151ea09f07bfb4d54ac
Alan luo <iteamalan@hotmail.com>
2015-12-06 16:04:27 +0800

    [Dashboard] : Add test case for configuration.

---
+++ SourceCode/Gatekeeper.DomainModel.Dashboard/WidgetItemModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gatekeeper.DomainModel.Dashboard
+{
+    public class WidgetItemModel
+    {
+        public string Text { get; set; }
+        public bool IsActive { get; set; }
+        public bool Selected { get; set; }
+    }
+}
